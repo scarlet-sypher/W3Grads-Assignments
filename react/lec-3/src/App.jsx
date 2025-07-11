123a6be9b35475cf567901a8053dd3137be1be54
Ayush <ayushjha21157@gmail.com>
2025-07-11 19:54:54 +0530

    some error is coming

--- react/lec-3/src/App.jsx
+++ react/lec-3/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
                 <h1 className='text-3xl font-bold text-blue-700 mb-4'>Counter APP</h1>
                 <p className='text-4xl font-mono mb-6'> {count} </p>
                 <div className='flex justify-between'>
-                    <button onClick={() => {setCount(count - 1)}} className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition'>  
+                    <button onClick={() => {setCount(count - 1)} } className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition'>  
                       Decrease
                     </button>
                      <button onClick={() => {setCount(0)}} className='bg-gray-400 text-white px-4 py-2 rounded hover:bg-gray-500 transition'>  
